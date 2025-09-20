a0daa34e6599eadca2b947a81aee2d15472d0d62
Suprcode <development@suprcode.com>
2025-09-20 23:56:39 +0100

    SETHERMITSTAT command

---
+++ ServerLibrary/Envir/Commands/Command/Admin/SetHermitStat.cs
@@ -0,0 +1,27 @@
+﻿using Library;
+using Server.Models;
+using System;
+
+namespace Server.Envir.Commands.Command.Admin
+{
+    class SetHermitStat : AbstractParameterizedCommand<IAdminCommand>
+    {
+        public override string VALUE => "SETHERMITSTAT";
+        public override int PARAMS_LENGTH => 2;
+
+        public override void Action(PlayerObject player, string[] vals)
+        {
+            if (vals.Length < PARAMS_LENGTH)
+                ThrowNewInvalidParametersException(); //SetHermitStat Health 50
+
+            if (!Enum.TryParse(vals[1], true, out Stat tstat))
+                ThrowNewInvalidParametersException();
+            if (!int.TryParse(vals[2], out int tamount))
+                ThrowNewInvalidParametersException();
+
+            player.Character.HermitStats[tstat] = tamount;
+
+            player.RefreshStats();
+        }
+    }
+}
