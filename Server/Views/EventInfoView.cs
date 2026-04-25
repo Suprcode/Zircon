8196b4426f6953956f6c2fd10e1de173850469b5
Suprcode <development@suprcode.com>
2026-04-25 12:25:50 +0100

    Allow uncategorised regions to show in event list.

--- Server/Views/EventInfoView.cs
+++ Server/Views/EventInfoView.cs
@@ -19,14 +19,14 @@ namespace Server.Views
 
             WorldMonsterLookUpEdit.DataSource = SMain.Session.GetCollection<MonsterInfo>().Binding;
             WorldRespawnLookUpEdit.DataSource = SMain.Session.GetCollection<RespawnInfo>().Binding;
-            WorldRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.Area);
+            WorldRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.None || x.RegionType == RegionType.Area);
             WorldMapLookUpEdit.DataSource = SMain.Session.GetCollection<MapInfo>().Binding;
             WorldInstanceLookUpEdit.DataSource = SMain.Session.GetCollection<InstanceInfo>().Binding;
             WorldActionTypeLookUpEdit.DataSource = SEnvir.EventHandler.GetWorldEventActions();
 
             PlayerMonsterLookUpEdit.DataSource = SMain.Session.GetCollection<MonsterInfo>().Binding;
             PlayerRespawnLookUpEdit.DataSource = SMain.Session.GetCollection<RespawnInfo>().Binding;
-            PlayerRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.Area);
+            PlayerRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.None || x.RegionType == RegionType.Area);
             PlayerMapLookUpEdit.DataSource = SMain.Session.GetCollection<MapInfo>().Binding;
             PlayerInstanceLookUpEdit.DataSource = SMain.Session.GetCollection<InstanceInfo>().Binding;
             PlayerItemLookUpEdit.DataSource = SMain.Session.GetCollection<ItemInfo>().Binding;
@@ -34,7 +34,7 @@ namespace Server.Views
 
             MonsterMonsterLookUpEdit.DataSource = SMain.Session.GetCollection<MonsterInfo>().Binding;
             MonsterRespawnLookUpEdit.DataSource = SMain.Session.GetCollection<RespawnInfo>().Binding;
-            MonsterRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.Area);
+            MonsterRegionLookUpEdit.DataSource = SMain.Session.GetCollection<MapRegion>().Binding.Where(x => x.RegionType == RegionType.None || x.RegionType == RegionType.Area);
             MonsterMapLookUpEdit.DataSource = SMain.Session.GetCollection<MapInfo>().Binding;
             MonsterInstanceLookUpEdit.DataSource = SMain.Session.GetCollection<InstanceInfo>().Binding;
             MonsterItemLookUpEdit.DataSource = SMain.Session.GetCollection<ItemInfo>().Binding;
